02926c1fa5502ffd2771b574d57ee86e10f5b0fd
michael tio <77330912+michaeltio@users.noreply.github.com>
2023-11-17 20:10:35 +0700

    update footer

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -3,40 +3,34 @@ import LogoSekolah from "../Icon/school_logo 3.png";
 
 const Footer = () => {
     return (
-        <footer className="mt-8">
-            <div className="flex bg-[#0ea5e9] justify-between">
-                <div>
+        <footer className="text-white">
+            <div className="flex bg-[#1184e8] justify-between p-8">
+                <div className="flex items-center">
+                    <img
+                        src={LogoSekolah}
+                        alt="Logo Sekolah"
+                        className="w-20 mr-1 "
+                    />
                     <div>
-                        <img
-                            src={LogoSekolah}
-                            alt="Logo Sekolah"
-                            className="w-12 h-12 mr-1"
-                        />
-                    </div>
-                    <div>
-                        <div>
-                            <h1 className="font-extrabold">SMPK Mater Dei</h1>
-                        </div>
-                        <div>
-                            <p>Unggul</p>
-                            <p>Kasih</p>
-                            <p>Bermartabat</p>
-                        </div>
+                        <b>SMPK Mater Dei</b>
+                        <p>Unggul</p>
+                        <p>Kasih</p>
+                        <p>Bermartabat</p>
                     </div>
                 </div>
-                <div className="w-38">
-                    <h1>Alamat</h1>
-                    <p>
+                <div className="w-1/6">
+                    <h1 className="text-xl font-bold">Alamat</h1>
+                    <p className="text-xs">
                         Jl. Pamulang Permai, Pamulang Bar., Pamulang, Kota
                         Tangerang Selatan, Banten 15417, Indonesia.
                     </p>
                 </div>
                 <div>
-                    <h1 className="font-extrabold">Kontak</h1>
-                    <p>(Phone) 021-7407148</p>
+                    <h1 className="font-extrabold text-xl font-bold">Kontak</h1>
+                    <p className="text-xs">(Phone) 021-7407148</p>
                 </div>
-                <div className="w-32">
-                    <p>
+                <div className="w-1/6">
+                    <p className="text-xs">
                         Sebagai lembaga pendidikan yang telah terbukti
                         memberikan layanan sesuai dengan kebutuhan pasar dan
                         menghasilkan generasi muda yang berprestasi, Sekolah
@@ -47,9 +41,8 @@ const Footer = () => {
                     </p>
                 </div>
             </div>
-
-            <div className="bg-[#1e40af]">
-                <p>&copy; Copyright 2023</p>
+            <div className="bg-[#0f75ce] pt-1 pb-1">
+                <p className="text-center">&copy; Copyright 2023</p>
             </div>
         </footer>
     );
