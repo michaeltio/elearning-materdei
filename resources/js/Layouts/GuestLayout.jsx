d70a0692ec99d984ee46e9b5eebcdcde17ebd0a5
michael tio <77330912+michaeltio@users.noreply.github.com>
2023-11-22 02:23:00 +0700

    update: benerin Login page

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -15,6 +15,23 @@ export default function Guest({ className = "", children }) {
                 src={HomeBackground}
             />
             <main className={className}>{children}</main>
+            <div className="fixed bottom-0 bg-primaryBlue w-full text-white flex justify-between text-center p-2 items-center">
+                <div className="flex items-center">
+                    <img src={LogoMaterDei} alt="" className="w-16" />
+                    <h1>E-Learning SMPK Mater Dei</h1>
+                </div>
+                <div>
+                    <h1>Contact</h1>
+                    <p>SMPK Mater Dei 123-456-890</p>
+                </div>
+                <div>
+                    <h1>Alamat</h1>
+                    <p>
+                        Jl. Pamulang Permai, Pamulang Bar., Pamulang, Kota
+                        Tangerang Selatan, Banten 15417, Indonesia.
+                    </p>
+                </div>
+            </div>
         </>
     );
 }
