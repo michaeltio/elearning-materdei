46d80c2d980feb93be68876d9f7f81034b7a6a5e
NAS2203 <139289057+H0tt4ru@users.noreply.github.com>
2023-11-25 15:40:44 +0700

    Attendance Fix

--- resources/js/Pages/Student/Attendance.jsx
+++ resources/js/Pages/Student/Attendance.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Head } from "@inertiajs/react";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 
@@ -7,31 +7,42 @@ import AttendanceIcon from "/public/Assets/attendance-icon.svg";
 import CheckIcon from "/public/Assets/check-icon.svg";
 export default function Attendance({ auth }) {
     const [isAttend, setIsAttend] = useState(false);
+    const [date, setDate] = useState("");
 
     const handleAttend = () => {
         setIsAttend(true);
     };
+
+    useEffect(() => {
+        const currentDate = new Date();
+        const formattedDate = currentDate.toLocaleDateString("id-ID", {
+            weekday: "long",
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+        });
+        setDate(formattedDate);
+    }, []);
+
     return (
         <AuthenticatedLayout user={auth.user}>
             <Head title="attendance" />
-            {/* page */}
-            <div>
-                <div className="text-center bg-primaryBlue rounded-xl w-64 md:w-96 mx-auto text-white mt-8 md:mt-28 p-4">
-                    <h1>30 January 1993</h1>
+            <div className="flex flex-col">
+                <div className="text-center bg-primaryBlue rounded-xl w-64 text-white p-4">
+                    <h1>{date}</h1>
                     <h1>{auth.user.user_details.full_name}</h1>
-                    <h1>Status : Attended</h1>
+                    <h1>
+                        Status : {isAttend == true ? "Attended" : "Absence"}
+                    </h1>
                     <div
                         className={`w-full h-4 rounded-xl ${
                             isAttend ? "bg-green-500" : "bg-red-600"
                         }`}
                     ></div>
                 </div>
-                <button
-                    className="w-64 h-64 rounded-full bg-primaryBlue transform translate-x-[-50%] translate-y-[-50%] absolute top-1/2 left-1/2 hover:bg-secondaryBlue "
-                    onClick={handleAttend}
-                >
+                <button className="mt-8 flex flex-col rounded-full justify-center items-center bg-primaryBlue w-64 h-64">
                     <img
-                        className="w-32 mx-auto brightness-0 invert"
+                        className="flex w-32 brightness-0 invert"
                         src={isAttend ? CheckIcon : AttendanceIcon}
                         alt=""
                     />
--- resources/js/Pages/Student/Home.jsx
+++ resources/js/Pages/Student/Home.jsx
@@ -62,31 +62,35 @@ export default function SHome({ auth }) {
     return (
         <AuthenticatedLayout user={auth.user}>
             <Head title="Home" />
-            <p className="flex justify-center items-center mt-12 text-black text-2xl ">
-                <span className="border-b border-black">Subject Overview</span>
-            </p>
-            <div className="flex flex-col items-center justify-center mt-3 sm:flex-row sm:justify-end">
-                <input
-                    type="text"
-                    placeholder="Search"
-                    className="mb-2 sm:mb-0 sm:mr-3 rounded-md"
-                />
-                <img
-                    src={SearchIcon}
-                    alt="Search Icon"
-                    className="w-5 h-5 mb-2 sm:mb-0 sm:mr-4"
-                />
-            </div>
-
-            <div className="flex flex-wrap mt-12">
-                {cards.map((card, index) => (
-                    <SubjectCard
-                        key={index}
-                        bgColor={card.bgColor}
-                        content={card.content}
-                        arrowColor={card.arrowColor}
+            <div>
+                <p className="flex justify-center items-center mt-12 text-black text-2xl ">
+                    <span className="border-b border-black">
+                        Subject Overview
+                    </span>
+                </p>
+                <div className="flex flex-col items-center justify-center mt-3 sm:flex-row sm:justify-end">
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        className="mb-2 sm:mb-0 sm:mr-3 rounded-md"
                     />
-                ))}
+                    <img
+                        src={SearchIcon}
+                        alt="Search Icon"
+                        className="w-5 h-5 mb-2 sm:mb-0 sm:mr-4"
+                    />
+                </div>
+
+                <div className="flex flex-wrap mt-12">
+                    {cards.map((card, index) => (
+                        <SubjectCard
+                            key={index}
+                            bgColor={card.bgColor}
+                            content={card.content}
+                            arrowColor={card.arrowColor}
+                        />
+                    ))}
+                </div>
             </div>
         </AuthenticatedLayout>
     );
