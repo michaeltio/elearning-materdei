6b703b7a3954660194fdf93692a3bdeb847c3c83
NAS2203 <139289057+H0tt4ru@users.noreply.github.com>
2023-12-21 18:51:22 +0700

    Update Subject.php

--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -9,7 +9,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Subject extends Model
 {
     use HasFactory;
-    protected $primaryKey = 'subjectId';
     
     public function classSubjects()
     {
